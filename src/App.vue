804c305954b57179bef2d425fcdc222f09f1d0b3
eno <sutrisnoeno53@gmail.com>
2020-04-24 22:55:41 +0700

    test stylus

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
     <div class="container">
         <div class="content">
-            <h3>Eno</h3>
+            <div class="col-xl-7">
+                <div><p>&#8592; Back to cart</p></div>
+                <div class="col-xl-5">
+                    <input type="checkbox" id="dropshipper" name="dropshipper">
+                    <label for="dropshipper"> Send as dropshipper</label><br>
+                </div>
+                <div class="col-xl-5 label">Delivery details</div>
+            </div>
+            <div class="col-xl-3">Column left</div>
         </div>
     </div>
 </template>
